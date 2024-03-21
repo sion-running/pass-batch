c96866f2480c7c39144a4feb0f848cd671b08c2b
sion-running <staythismoment.sion@gmail.com>
2024-03-21 22:26:41 +0900

    이용권 추가 관련 tasklet기반 배치 job 구현

---
+++ src/main/java/com/fastcamplus/pass/repository/pass/PassModelMapper.java
@@ -0,0 +1,24 @@
+package com.fastcamplus.pass.repository.pass;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+// ReportingPolicy.IGNORE: 일치하지 않은 필드는 무시
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PassModelMapper {
+    PassModelMapper INSTANCE = Mappers.getMapper(PassModelMapper.class);
+
+    // 필드명이 같지 않거나 custom하게 매핑해주기 위해서는 @Mapping을 추가해준다
+    @Mapping(target = "status", qualifiedByName = "defaultStatus")
+    @Mapping(target = "remainingCount", source = "bulkPassEntity.count")
+    PassEntity toPassEntity(BulkPassEntity bulkPassEntity, String userId);
+
+    // BulkPassStatus와 관계 없이 PassStatus 값을 설정
+    @Named("defaultStatus")
+    default PassStatus status(BulkPassStatus status) {
+        return PassStatus.READY;
+    }
+}
